8dbafd2ea0f269e336fa3ce96548cacada3ffba6
weixi-hash <sanyc_2023@outlook.com>
2026-08-14 15:58:20 +0800

    feat: add PWA support for mobile install

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'snake-game-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
